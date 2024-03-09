b11741981abac914f489b566e6d3b2fa42bcda42
jxpsert <jplatenburg@outlook.com>
2024-03-09 20:50:34 +0100

    Schedule for import

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call('App\Http\Controllers\API\DefibrillatorController@import')->dailyAt('08:00');
     }
 
     /**
@@ -20,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
