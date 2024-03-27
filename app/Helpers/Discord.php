0ed4fe9b47e32edd71ecf2be5b8878909af320e5
jxpsert <jplatenburg@outlook.com>
2024-03-27 19:35:37 +0100

    Laatst toegevoegde AEDs in Discord bericht

--- app/Helpers/Discord.php
+++ app/Helpers/Discord.php
@@ -6,14 +6,22 @@ class Discord
 {
     public static function syncFinished($totalSynced)
     {
-        if (!config('app.webhooks.sync')) return true;
+        if (!config('app.webhooks.sync'))
+            return true;
         $url = config('app.webhooks.sync');
         $username = config('app.name');
+
+        $lastAdded = \App\Models\Defibrillator::orderBy('id', 'desc')->limit(3)->get();
+        $lastAdded = $lastAdded->map(function ($item) {
+            return "[{$item->city}, {$item->province}](https://openstreetmap.org/node/{$item->osm_id})";
+        })->implode("\n");
+
+        $lastAdded = "**Laatst toegevoegde AEDs**\n$lastAdded";
 
         $data = [
             'username' => $username,
             'embeds' => [
-                Discord::createEmbed('AED synchronisatie afgerond', 'Totaal ' . $totalSynced . ' AEDs')
+                Discord::createEmbed('AED synchronisatie afgerond', "Totaal $totalSynced AEDs\n\n" . $lastAdded),
             ],
         ];
 
@@ -22,7 +30,8 @@ class Discord
 
     public static function syncStarted()
     {
-        if (!config('app.webhooks.sync')) return true;
+        if (!config('app.webhooks.sync'))
+            return true;
         $url = config('app.webhooks.sync');
         $username = config('app.name');
 
