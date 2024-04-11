3affbadadc5dd3fde68b816e13b984e09c9442b6
jxpsert <jplatenburg@outlook.com>
2024-04-11 21:35:20 +0200

    Dynamisch inladen aed info

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -111,7 +111,7 @@
                 popupAnchor: [0, -40]
             });
 
-            const allAEDURL = "{{ route('api.aed.all') }}";
+            const allAEDURL = "{{ route('api.aed.basic') }}";
             fetch(allAEDURL)
                 .then(response => response.json())
                 .then(data => {
@@ -122,9 +122,7 @@
                 aedMarkerGroup.clearLayers(); // Empty so we can refill it
 
                 aeds.forEach(aed => {
-                    let icon, access, indoor, level = "",
-                        locked = "",
-                        cabinet = "";
+                    let icon;
 
                     if (aed.access == "permissive") {
                         icon = permissiveIcon;
@@ -140,111 +138,32 @@
                         access = "{{ __('defibrillator.access.public') }}"
                     }
 
-                    if (aed.indoor == true) {
-                        indoor = "{{ __('Yes') }}";
-                        if (aed.level != 0 && aed.level != null) {
-                            level = ("{{ __('defibrillator.indoor.on_floor', ['floor' => '-level-']) }}")
-                                .replace('-level-', aed.level);
-                        } else if (aed.level == null) {
-                            level = "";
-                        } else {
-                            level = "{{ __('defibrillator.indoor.on_ground_floor') }}";
+                    const aedMarker = L.Marker.extend({
+                        options: {
+                            id: null
                         }
-                    } else {
-                        indoor = "{{ __('No') }}";
-                    }
+                    });
 
-                    if (aed.locked == "yes") {
-                        locked = "{{ __('Yes') }}";
-                    } else {
-                        locked = "{{ __('No') }}";
-                    }
+                    const marker = new aedMarker([aed.latitude, aed.longitude], {
+                        icon: icon,
+                        id: aed.id
+                    });
 
-                    switch (aed.cabinet) {
-                        case "horizontal_door":
-                            cabinet = "{{ __('defibrillator.cabinet.horizontal_door') }}";
-                            break;
-                        case "vertical_door":
-                            cabinet = "{{ __('defibrillator.cabinet.vertical_door') }}";
-                            break;
-                        case "no":
-                            cabinet = "{{ __('defibrillator.cabinet.no') }}";
-                            break;
-                        case "twist":
-                            cabinet = "{{ __('defibrillator.cabinet.twist') }}";
-                            break;
-                        case "mechanical":
-                            cabinet = "{{ __('defibrillator.cabinet.mechanical') }}";
-                            break;
-                        default:
-                            cabinet = "{{ __('defibrillator.cabinet.unknown') }}";
-                            break;
-                    }
+                    marker.on('click', function(e) {
+                        const id = e.target.options.id;
 
-                    let popupContent = ''; // Initialise popup content
-                    popupContent +=
-                        `<h3>${aed.operator ?? "{{ __('defibrillator.operator.unknown') }}"}</h3>`; // Operator name
-                    if (aed.operator_website) popupContent +=
-                        `<small><a target='_blank' href='${aed.operator_website}'>${aed.operator_website}</a></small><br>`; // Operator website
-                    popupContent += `<b>{{ __('defibrillator.access') }}:</b> ${access}`; // Access
-                    if (aed.locked) popupContent +=
-                        `<br><b>{{ __('defibrillator.locked') }}:</b> ${aed.locked ? "{{ __('Yes') }}" : "{{ __('No') }}"}`; // Lock
-                    if (aed.phone) popupContent +=
-                        `<br><b>{{ __('defibrillator.phone') }}:</b> ${aed.phone}`; // Operator phone number
-                    if (aed.location) popupContent +=
-                        `<br><b>{{ __('defibrillator.location') }}:</b> ${aed.location}`; // Exact location
-                    popupContent +=
-                        `<br><b>{{ __('defibrillator.indoor') }}:</b> ${indoor}${level}`; // Whether the AED is indoors
-                    if (aed["opening_hours"]) popupContent +=
-                        `<br><b>{{ __('defibrillator.opening_hours') }}:</b>${parseOpeningHours(aed.opening_hours)}`; // Opening hours
-                    if (aed.manufacturer) popupContent +=
-                        `<br><b>{{ __('defibrillator.type') }}:</b> ${aed.manufacturer} ${aed.model ?? ""}`; // Manufacturer
-                    if (aed.cabinet) popupContent +=
-                        `<br><b>{{ __('defibrillator.cabinet') }}:</b> ${cabinet}`; // Case
-                    if (aed.cabinet_manufacturer) popupContent +=
-                        `<br><b>{{ __('defibrillator.cabinet_manufacturer') }}:</b> ${aed.cabinet_manufacturer}`; // Case manufacturer
-                    if (aed.image) {
-                        let directurl = aed.image;
-                        if (directurl.includes("commons.wikimedia.org")) { // Wikimedia Commons
-                            let filename = directurl.substring(directurl.lastIndexOf("File:") + 5,
-                                directurl.length);
-                            directurl = "https://commons.wikimedia.org/wiki/Special:FilePath/" +
-                                filename;
-                        }
-                        popupContent +=
-                            `<br><a target='_blank' href='${directurl}'><img class="popupimage" src='${directurl}'></a>`; // Image
-                    }
-                    if (aed.description) popupContent +=
-                        `<br><b>{{ __('defibrillator.description') }}:</b> ${aed.description}`; // Description
-                    popupContent +=
-                        `<br><a target='_blank' href='https://www.openstreetmap.org/node/${aed.osm_id}'>{{ __('defibrillator.edit_on_osm') }}</a>`; // OSM link
+                        let url = "{{ route('api.aed.one', ['id' => 'id']) }}";
+                        url = url.replace('id', id);
+                        fetch(url)
+                            .then(response => response.json())
+                            .then(data => {
+                                e.target.bindPopup(getPopupContent(data.result)).openPopup();
+                            });
+                    });
 
-                    aedMarkerGroup.addLayer(L.marker([aed.latitude, aed.longitude], {
-                        icon: icon
-                    }).bindPopup(
-                        popupContent));
+                    aedMarkerGroup.addLayer(marker)
+                    // .bindPopup(popupContent));
                 });
-            }
-
-            function parseOpeningHours(hours) {
-                let returnstring = "";
-
-                hours = hours.replace("Mo", "{{ __('days.mo') }}");
-                hours = hours.replace("Tu", "{{ __('days.tu') }}");
-                hours = hours.replace("We", "{{ __('days.we') }}");
-                hours = hours.replace("Th", "{{ __('days.th') }}");
-                hours = hours.replace("Fr", "{{ __('days.fr') }}");
-                hours = hours.replace("Sa", "{{ __('days.sa') }}");
-                hours = hours.replace("Su", "{{ __('days.su') }}");
-
-                let strings = hours.split(";");
-                strings.forEach(timestring => {
-                    if (timestring.includes("off")) return;
-
-                    returnstring += "<br>" + timestring;
-                });
-
-                return returnstring;
             }
         });
 
@@ -276,6 +195,124 @@
 
                 localStorage.setItem("legend", true);
             }
+        }
+
+        const parseOpeningHours = (hours) => {
+            let returnstring = "";
+
+            hours = hours.replace("Mo", "{{ __('days.mo') }}");
+            hours = hours.replace("Tu", "{{ __('days.tu') }}");
+            hours = hours.replace("We", "{{ __('days.we') }}");
+            hours = hours.replace("Th", "{{ __('days.th') }}");
+            hours = hours.replace("Fr", "{{ __('days.fr') }}");
+            hours = hours.replace("Sa", "{{ __('days.sa') }}");
+            hours = hours.replace("Su", "{{ __('days.su') }}");
+
+            let strings = hours.split(";");
+            strings.forEach(timestring => {
+                if (timestring.includes("off")) return;
+
+                returnstring += "<br>" + timestring;
+            });
+
+            return returnstring;
+        }
+
+        const getPopupContent = (aed) => {
+            let access, indoor, level = "",
+                locked = "",
+                cabinet = "";
+
+            if (aed.access == "permissive") {
+                access = "{{ __('defibrillator.access.permissive') }}"
+            } else if (aed.access == "unknown" || aed.access == null) {
+                access = "{{ __('defibrillator.access.unknown') }}"
+            } else if (aed.access == "private" || aed.access == "no") {
+                access = "{{ __('defibrillator.access.private') }}"
+            } else {
+                access = "{{ __('defibrillator.access.public') }}"
+            }
+
+            if (aed.indoor == true) {
+                indoor = "{{ __('Yes') }}";
+                if (aed.level != 0 && aed.level != null) {
+                    level = ("{{ __('defibrillator.indoor.on_floor', ['floor' => '-level-']) }}")
+                        .replace('-level-', aed.level);
+                } else if (aed.level == null) {
+                    level = "";
+                } else {
+                    level = "{{ __('defibrillator.indoor.on_ground_floor') }}";
+                }
+            } else {
+                indoor = "{{ __('No') }}";
+            }
+
+            if (aed.locked == "yes") {
+                locked = "{{ __('Yes') }}";
+            } else {
+                locked = "{{ __('No') }}";
+            }
+
+            switch (aed.cabinet) {
+                case "horizontal_door":
+                    cabinet = "{{ __('defibrillator.cabinet.horizontal_door') }}";
+                    break;
+                case "vertical_door":
+                    cabinet = "{{ __('defibrillator.cabinet.vertical_door') }}";
+                    break;
+                case "no":
+                    cabinet = "{{ __('defibrillator.cabinet.no') }}";
+                    break;
+                case "twist":
+                    cabinet = "{{ __('defibrillator.cabinet.twist') }}";
+                    break;
+                case "mechanical":
+                    cabinet = "{{ __('defibrillator.cabinet.mechanical') }}";
+                    break;
+                default:
+                    cabinet = "{{ __('defibrillator.cabinet.unknown') }}";
+                    break;
+            }
+
+            let popupContent = ''; // Initialise popup content
+            popupContent +=
+                `<h3>${aed.operator ?? "{{ __('defibrillator.operator.unknown') }}"}</h3>`; // Operator name
+            if (aed.operator_website) popupContent +=
+                `<small><a target='_blank' href='${aed.operator_website}'>${aed.operator_website}</a></small><br>`; // Operator website
+            popupContent += `<b>{{ __('defibrillator.access') }}:</b> ${access}`; // Access
+            if (aed.locked) popupContent +=
+                `<br><b>{{ __('defibrillator.locked') }}:</b> ${aed.locked ? "{{ __('Yes') }}" : "{{ __('No') }}"}`; // Lock
+            if (aed.phone) popupContent +=
+                `<br><b>{{ __('defibrillator.phone') }}:</b> ${aed.phone}`; // Operator phone number
+            if (aed.location) popupContent +=
+                `<br><b>{{ __('defibrillator.location') }}:</b> ${aed.location}`; // Exact location
+            popupContent +=
+                `<br><b>{{ __('defibrillator.indoor') }}:</b> ${indoor}${level}`; // Whether the AED is indoors
+            if (aed["opening_hours"]) popupContent +=
+                `<br><b>{{ __('defibrillator.opening_hours') }}:</b>${parseOpeningHours(aed.opening_hours)}`; // Opening hours
+            if (aed.manufacturer) popupContent +=
+                `<br><b>{{ __('defibrillator.type') }}:</b> ${aed.manufacturer} ${aed.model ?? ""}`; // Manufacturer
+            if (aed.cabinet) popupContent +=
+                `<br><b>{{ __('defibrillator.cabinet') }}:</b> ${cabinet}`; // Case
+            if (aed.cabinet_manufacturer) popupContent +=
+                `<br><b>{{ __('defibrillator.cabinet_manufacturer') }}:</b> ${aed.cabinet_manufacturer}`; // Case manufacturer
+            if (aed.image) {
+                let directurl = aed.image;
+                if (directurl.includes("commons.wikimedia.org")) { // Wikimedia Commons
+                    let filename = directurl.substring(directurl.lastIndexOf("File:") + 5,
+                        directurl.length);
+                    directurl = "https://commons.wikimedia.org/wiki/Special:FilePath/" +
+                        filename;
+                }
+                popupContent +=
+                    `<br><a target='_blank' href='${directurl}'><img class="popupimage" src='${directurl}'></a>`; // Image
+            }
+            if (aed.description) popupContent +=
+                `<br><b>{{ __('defibrillator.description') }}:</b> ${aed.description}`; // Description
+            popupContent +=
+                `<br><a target='_blank' href='https://www.openstreetmap.org/node/${aed.osm_id}'>{{ __('defibrillator.edit_on_osm') }}</a>`; // OSM link
+
+            return popupContent;
         }
     </script>
 @endsection
