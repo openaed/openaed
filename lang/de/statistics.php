7e442d366336ea9a068dd33c4f97afa6367c01ea
jxpsert <jplatenburg@outlook.com>
2024-05-25 14:38:05 +0200

    AED-4 Renamed 'province' to 'region' for internationalisation

--- lang/de/statistics.php
+++ lang/de/statistics.php
@@ -2,7 +2,7 @@
 
 return [
     'current' => 'Aktuell enthält die Datenbank :amount Defibrillatoren in :cities verschiedenen Städten.',
-    'per_province' => 'Provinzweise',
+    'per_province' => 'Regional',
     'per_city' => 'Stadtweise',
     'per_operator' => 'Betreiberweise',
     'province' => 'Provinz',
