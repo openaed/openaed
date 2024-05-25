de85b38348499ffaa8cf9c322f10af9cbabad457
jxpsert <jplatenburg@outlook.com>
2024-05-25 14:36:35 +0200

    AED-4 Renamed 'province' to 'region' for internationalisation

--- lang/en/statistics.php
+++ lang/en/statistics.php
@@ -2,7 +2,7 @@
 
 return [
     'current' => 'At this moment, the database consists of :amount AEDs in :cities different cities.',
-    'per_province' => 'Per province',
+    'per_province' => 'Per region',
     'per_city' => 'Per city',
     'per_operator' => 'Per operator',
     'province' => 'Province',
--- lang/nl/statistics.php
+++ lang/nl/statistics.php
@@ -2,7 +2,7 @@
 
 return [
     'current' => 'Op dit moment staan er :amount AED\'s in de database in :cities verschillende woonplaatsen.',
-    'per_province' => 'Per provincie',
+    'per_province' => 'Per regio',
     'per_city' => 'Per woonplaats',
     'per_operator' => 'Per beheerder',
     'province' => 'Provincie',
